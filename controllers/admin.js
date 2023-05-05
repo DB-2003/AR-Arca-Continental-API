acb4939ed974ef2526fa5b0b684c47f59ea62bac
MichelleBG18 <A00833416@itesm.mx>
2023-05-05 09:25:14 -0600

    Terminacion Admin y Primera Página chofer

--- controllers/admin.js
+++ controllers/admin.js
@@ -78,4 +78,158 @@ module.exports = {
           .json({ message: `Error al obtener la informacion del chofer. Err:, ${error}` });
       }
     },
+
+    getInfoSolicitudAceptada: async(req, res, next) => {
+      try {
+          var userId = req.params.id;
+          userId = parseInt(userId, 10)
+          var id_solicitud = req.params.id_solicitud
+          id_solicitud = parseInt(id_solicitud, 10)
+          user = await UserServices.getSolicitudAceptada(userId, id_solicitud)
+          
+          return res.status(200).json(user);
+      } catch (error) {
+      return res
+          .status(500)
+          .json({ message: `Error al obtener el refrigerador. Err:, ${error}` });
+      }
+    },
+
+    insertRelleno: async(req,res,next)=>{
+      let {
+        id_chofer,
+        comentario,
+        id_solicitud
+      } = req.body;
+  
+      try {  
+        const data = await UserServices.insertAdminSolicitud(
+          id_chofer,
+          comentario,
+          id_solicitud);
+  
+        if (data < 1) {
+          // return an error response if insertSolicitud() returns falsy value
+          return res.status(400).json({
+            success: false,
+            message: 'Failed to insert datos solicitud por admin'
+          });
+        }
+  
+        res.status(200).json({
+          success: true,
+          result: data[0]
+  
+          // id_solicitud,
+        })
+      } catch (error) {
+        // handle any errors that occur during the try block
+        console.error(error);
+        res.status(500).json({
+          success: false,
+          message: 'Server error'
+        });
+      }
+    },
+
+    getKPIAdmin: async(req, res, next) => {
+      try {
+          var userId = req.params.id;
+          userId = parseInt(userId, 10)
+          user = await UserServices.getKPIAdminService(userId)
+          
+          return res.status(200).json(user);
+      } catch (error) {
+      return res
+          .status(500)
+          .json({ message: `Error al obtener la solicitud. Err:, ${error}` });
+      }
+    },
+
+    deleteSolicitud: async(req,res,next)=>{
+      let {
+        id_solicitud
+      } = req.body;
+  
+      try {  
+        const data = await UserServices.delete(id_solicitud);
+  
+        if (data < 1) {
+          // return an error response if insertSolicitud() returns falsy value
+          return res.status(400).json({
+            success: false,
+            message: 'Failed to insert datos solicitud por admin'
+          });
+        }
+  
+        res.status(200).json({
+          success: true,
+          result: data[0]
+  
+          // id_solicitud,
+        })
+      } catch (error) {
+        // handle any errors that occur during the try block
+        console.error(error);
+        res.status(500).json({
+          success: false,
+          message: 'Server error'
+        });
+      }
+    },
+
+    getHeaderChofer: async (req, res, next) => {
+      try {
+        var userId = req.params.id;
+        userId = parseInt(userId, 10);
+        user = await UserServices.getHeadChofer(userId);
+  
+        if (!user) {
+          return res
+            .status(404)
+            .json({
+              message: "Supervisor por id no encontrado",
+              success: false,
+              found: false,
+            });
+          }
+  
+        return res.status(200).json(user);
+  
+        } catch (error) {
+          return res
+              .status(500)
+              .json({ message: `Error al obtener la informacion. Err:, ${error}` });
+          }
+        },
+
+        getInfoRefriChof: async(req, res, next) => {
+          try {
+              var userId = req.params.id;
+              userId = parseInt(userId, 10)
+              var id_solicitud = req.params.id_solicitud
+              id_solicitud = parseInt(id_solicitud, 10)
+              user = await UserServices.getRefriChof(userId, id_solicitud)
+              
+              return res.status(200).json(user);
+          } catch (error) {
+          return res
+              .status(500)
+              .json({ message: `Error al obtener el refrigerador. Err:, ${error}` });
+          }
+        },
+
+        getSoli: async(req, res, next) => {
+          try {
+              var userId = req.params.id;
+              userId = parseInt(userId, 10)
+              user = await UserServices.getSol(userId)
+              
+              return res.status(200).json(user);
+          } catch (error) {
+          return res
+              .status(500)
+              .json({ message: `Error al obtener la solicitud. Err:, ${error}` });
+          }
+        },
 };
--- routes/admin.js
+++ routes/admin.js
@@ -7,5 +7,13 @@ router.get('/datos/:id', UserControllers.getInfoAdmin)
 router.get('/solicitudes/:id', UserControllers.getInfoSolicitud)
 router.get('/refrigeradores/solicitud/:id/:id_solicitud', UserControllers.getInfoRefrigerador)
 router.get('/informacion/chofer', UserControllers.getInfoChofer)
+router.get('/general/:id/:id_solicitud', UserControllers.getInfoSolicitudAceptada)
+router.post('/relleno/solicitud', UserControllers.insertRelleno)
+router.get('/kpiadmin/:id', UserControllers.getKPIAdmin)
+router.post('/delete', UserControllers.deleteSolicitud)
+router.get('/headerchofer/:id', UserControllers.getHeaderChofer)
+router.get('/refri/chof/:id/:id_solicitud', UserControllers.getInfoRefriChof)
+router.get('/soli/:id', UserControllers.getSoli)
+
 
 module.exports = router;
